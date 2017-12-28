04ccf45a425f09eff0261ee3b32bf3cf770afbbe
pierrelevy <pierrelevy@users.noreply.github.com>
2017-12-28 11:56:25 +0100

    BOTTICKET-4 : Upgrade for EasyRuleBot 1.0.0

--- src/java/fr/paris/lutece/plugins/easyrulesbot/modules/ticketing/business/TicketingBot.java
+++ src/java/fr/paris/lutece/plugins/easyrulesbot/modules/ticketing/business/TicketingBot.java
@@ -1,58 +1,56 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2002-2017, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
  */
+
 package fr.paris.lutece.plugins.easyrulesbot.modules.ticketing.business;
 
-import fr.paris.lutece.plugins.easyrulesbot.business.Bot;
-import fr.paris.lutece.portal.service.util.AppLogService;
-import fr.paris.lutece.portal.service.util.AppPropertiesService;
-import fr.paris.lutece.util.httpaccess.HttpAccess;
-import fr.paris.lutece.util.httpaccess.HttpAccessException;
-import java.util.HashMap;
+import fr.paris.lutece.plugins.easyrulesbot.service.EasyRulesBot;
 import java.util.Locale;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 
 /**
  *
  * @author closea
  */
-public class TicketingBot extends Bot 
+public class TicketingBot extends EasyRulesBot 
 {
-    private static final String PROPERTY_LAST_MESSAGE = "module.easyrulesbot.facilfamilles.lastMessage";
-    private static final String PROPERTY_URL_TICKETING = "easyrulesbot-ticketing.url.ticketing";
-
     /**
      * {@inheritDoc }
      */
     @Override
     public String processData( HttpServletRequest request, Map<String, String> mapData, Locale locale )
     {
-        /*
-        Map<String,String> mapParameters = new HashMap<String,String>();
-        HttpAccess httpAccess = new HttpAccess();
-        mapParameters.put(  "page", "ticket" );
-        mapParameters.put(  "action", "createTicket"  );
-        mapParameters.put(  "", "" );
-        mapParameters.put(  "", "" );
-        mapParameters.put(  "", "" );
-        mapParameters.put(  "", "" );
-        mapParameters.put(  "", "" );
-        
-        String strUrl = AppPropertiesService.getProperty( PROPERTY_URL_TICKETING );
-        try
-        {
-            httpAccess.doPost( strUrl , mapParameters );
-        }
-        catch( HttpAccessException ex )
-        {
-            AppLogService.error( "Error ", ex );
-        }
-        */
         return "Votre demande a été envoyée";
     }
 }
